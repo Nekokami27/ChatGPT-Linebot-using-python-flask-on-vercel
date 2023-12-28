9621d81c90275647d4c9abaf76cdd4bbae2ddf34
Nekokami <43298206+Nekokami27@users.noreply.github.com>
2023-12-28 19:21:21 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,3 +1,12 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage
+from api.chatgpt import ChatGPT
+import threading
+import os
+import queue
+
 line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
 line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
 working_status = os.getenv("DEFAULT_TALKING", default="true").lower() == "true"
