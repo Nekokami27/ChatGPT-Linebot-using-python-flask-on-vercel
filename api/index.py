27422764989d8b1452cdddaf4c3ec2a710809e1f
Nekokami <43298206+Nekokami27@users.noreply.github.com>
2023-12-28 16:38:07 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -30,25 +30,8 @@ def callback():
             line_handler.handle(body, signature)
         except InvalidSignatureError:
             abort(400)
-    thread = threading.Thread(target=handle)
-    thread.start()
-
-    # 立即回應
+    threading.Thread(target=handle).start()
     return 'OK'
-    '''
-    # get X-Line-Signature header value
-    signature = request.headers['X-Line-Signature']
-    # get request body as text
-    body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)
-    # handle webhook body
-    try:
-        line_handler.handle(body, signature)
-    except InvalidSignatureError:
-        abort(400)
-    return 'OK'
-  '''  
-
 
 @line_handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
@@ -56,7 +39,7 @@ def handle_message(event):
     if event.message.type != "text":
         return
         
-    if event.message.text.startswith('Hi'):
+    if event.message.text.lower().startswith('hi'):
         chatgpt.add_msg(f"HUMAN:{event.message.text}?\n")
         reply_msg = chatgpt.get_response().replace("AI:", "", 1)
         chatgpt.add_msg(f"AI:{reply_msg}\n")
@@ -64,11 +47,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text=event.message.text)
         )
-        '''
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=reply_msg))
-    '''
+
 """
     if event.message.text == "說話":
         working_status = True
